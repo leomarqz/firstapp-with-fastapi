1426b5f4c63c1585444d17e63159f19bd436b253
Elmer Marquez <leomarqz.main@gmail.com>
2025-12-25 17:09:40 -0600

    refact: se realizo una refactorizacion de codigo en los endpoints de transactions

--- app/routers/transaction.py
+++ app/routers/transaction.py
@@ -9,32 +9,27 @@ router = APIRouter()
 
 @router.get("/transactions")
 async def get_transactions(session: session_dependency):
-    try:
-        return session.exec( select(Transaction) ).all()
-    except Exception as e:
-        raise HTTPException(status_code=Status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+    return session.exec( select(Transaction) ).all()
 
 @router.get("/transactions/{transaction_id}")
 async def get_transaction(transaction_id: int, session: session_dependency):
-    try:
-        transaction_db = session.get(Transaction, transaction_id)
-        if not transaction_db:
-            raise HTTPException(status_code=Status.HTTP_404_NOT_FOUND, detail="Transaction not found")
-        return transaction_db
-    except Exception as e:
-        raise HTTPException(status_code=Status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+    transaction_db = session.get(Transaction, transaction_id)
+    if not transaction_db:
+        raise HTTPException(status_code=Status.HTTP_404_NOT_FOUND, detail="Transaction not found")
+    
+    return transaction_db
 
-@router.post("/transactions")
+@router.post("/transactions", status_code=Status.HTTP_201_CREATED, response_model=Transaction)
 async def create_transaction(transaction_data: TransactionCreate, session: session_dependency): 
-    try:
-        transaction_data_dict: dict = transaction_data.model_dump()
-        customer = session.get(Customer, transaction_data_dict.get("customer_id") )
-        if not customer:
-            raise HTTPException(status_code=Status.HTTP_404_NOT_FOUND, detail="Customer not found")
-        transaction_db = Transaction.model_validate( transaction_data_dict )
-        session.add(transaction_db)
-        session.commit()
-        session.refresh(transaction_db)
-        return transaction_db
-    except Exception as e:
-        raise HTTPException(status_code=Status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+    transaction_data_dict: dict = transaction_data.model_dump()
+    customer = session.get(Customer, transaction_data_dict.get("customer_id") )
+    
+    if not customer:
+        raise HTTPException(status_code=Status.HTTP_404_NOT_FOUND, detail="Customer doesn't exist!")
+    
+    transaction_db = Transaction.model_validate( transaction_data_dict )
+    session.add(transaction_db)
+    session.commit()
+    session.refresh(transaction_db)
+    
+    return transaction_db
